ba9442dfe96a8c7df3ea68b85bd4138914ed8afa
kiarieking <kiariekevin22@gmail.com>
2020-01-09 14:08:06 +0300

    moving cars and crash message

--- window.py
+++ window.py
@@ -1,7 +1,9 @@
 import pygame
 pygame.init()
 import time
-blue=(116,191,247)
+import random
+
+blue=(80,80,80)
 black=(0,0,0)
 display_width=800
 display_height=600
@@ -14,6 +16,23 @@ yellow_strip=pygame.image.load("yellow strip.jpg")
 strip=pygame.image.load("strip.jpg")
 car_width=53
 
+def obstacle(obs_startx,obs_starty,obs):
+	if obs==0:
+		obs_pic=pygame.image.load("car.jpg")
+	elif obs==1:
+		obs_pic=pygame.image.load("car1.jpg")
+	elif obs==2:
+		obs_pic=pygame.image.load("car2.jpg")
+	elif obs==3:
+		obs_pic=pygame.image.load("car4.jpg")
+	elif obs==4:
+		obs_pic=pygame.image.load("car5.jpg")
+	elif obs==5:
+		obs_pic=pygame.image.load("car6.jpg")
+	elif obs==7:
+		obs_pic=pygame.image.load("car7.jpg")
+
+	gamedisplays.blit(obs_pic,(obs_startx,obs_starty))	
 
 def text_objects(text,font):
 	textsurface=font.render(text,True,black)
@@ -59,13 +78,20 @@ def game_loop():
 	x=(display_width*0.45)
 	y=(display_height*0.8)
 	x_change=0
+	obstacle_speed=9
+	obs=0
+	y_change=0
+	obs_startx=random.randrange(200,(display_width-200))
+	obs_starty=-550
+	obs_width=56
+	obs_height=125
 
 	bumped=False
 	while not bumped:
 		for event in pygame.event.get():
 			if event.type==pygame.QUIT:
-				bumped=True
-
+				pygame.quit()
+				quit()
 
 		if event.type==pygame.KEYDOWN:
 			if event.key==pygame.K_LEFT:
@@ -80,9 +106,24 @@ def game_loop():
 
 		gamedisplays.fill(blue)
 		background()
+		obs_starty-=(obstacle_speed/4)
+		obstacle(obs_startx,obs_starty,obs)
+		obs_starty+=obstacle_speed
 		car(x,y)
+
 		if x>640-car_width or x<160:
 			crash()
+		if x>display_width-(car_width+140) or x<140:
+			crash()
+		if obs_starty>display_height:
+			obs_starty=0-obs_height
+			obs_startx=random.randrange(140,(display_width-140))
+			obs=random.randrange(0,7)
+
+		if y<obs_starty+obs_height:
+			if x>obs_startx and x<obs_startx+obs_width or x+car_width>obs_startx and x+car_width<obs_startx+obs_width:
+				crash()	
+
 		pygame.display.update()
 		clock.tick(60)
 
