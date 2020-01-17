61dbc9389ee508fd8dc7963d0dc428eb08812945
kiarieking <kiariekevin22@gmail.com>
2020-01-17 11:16:39 +0300

    score and level system- crash bug removed

--- window.py
+++ window.py
@@ -3,8 +3,9 @@ pygame.init()
 import time
 import random
 
-blue=(80,80,80)
+blue=(119,118,110)
 black=(0,0,0)
+red=(255,0,0)
 display_width=800
 display_height=600
 gamedisplays=pygame.display.set_mode((display_width,display_height))
@@ -20,7 +21,7 @@ def obstacle(obs_startx,obs_starty,obs):
 	if obs==0:
 		obs_pic=pygame.image.load("car.jpg")
 	elif obs==1:
-		obs_pic=pygame.image.load("car1.jpg")
+		 obs_pic=pygame.image.load("car1.jpg")
 	elif obs==2:
 		obs_pic=pygame.image.load("car2.jpg")
 	elif obs==3:
@@ -29,10 +30,17 @@ def obstacle(obs_startx,obs_starty,obs):
 		obs_pic=pygame.image.load("car5.jpg")
 	elif obs==5:
 		obs_pic=pygame.image.load("car6.jpg")
-	elif obs==7:
+	elif obs==6:
 		obs_pic=pygame.image.load("car7.jpg")
 
 	gamedisplays.blit(obs_pic,(obs_startx,obs_starty))	
+
+def score_system(score,passed):
+	font=pygame.font.SysFont(None,25)
+	text=font.render("Passed "+str(passed),True,black)
+	score=font.render("Score"+str(score),True,red)
+	gamedisplays.blit(text,(0,50))
+	gamedisplays.blit(score,(0,20))
 
 def text_objects(text,font):
 	textsurface=font.render(text,True,black)
@@ -85,6 +93,9 @@ def game_loop():
 	obs_starty=-550
 	obs_width=56
 	obs_height=125
+	passed=0
+	level=0
+	score=0
 
 	bumped=False
 	while not bumped:
@@ -98,7 +109,10 @@ def game_loop():
 				x_change=-5
 			if event.key==pygame.K_RIGHT:
 				x_change=5
-
+			if event.key==pygame.K_a:
+				obstacle_speed+=2
+			if event.key==pygame.K_b:
+				obstacle_speed-=2		
 		if event.type==pygame.KEYUP:
 			if event.key==pygame.K_LEFT or event.key==pygame.K_RIGHT:
 				x_change=0			
@@ -110,7 +124,7 @@ def game_loop():
 		obstacle(obs_startx,obs_starty,obs)
 		obs_starty+=obstacle_speed
 		car(x,y)
-
+		score_system(passed,score)
 		if x>640-car_width or x<160:
 			crash()
 		if x>display_width-(car_width+140) or x<140:
@@ -119,6 +133,17 @@ def game_loop():
 			obs_starty=0-obs_height
 			obs_startx=random.randrange(140,(display_width-140))
 			obs=random.randrange(0,7)
+			passed=passed+1
+			score=passed*10
+			if int(passed)%10==0:
+				level=level+1
+				obstacle_speed+=2
+				largetext=pygame.font.Font("freesansbold.ttf",80)
+				textsurf,textrect=text_objects("Level "+str(level),largetext)
+				textrect.center=((display_width/2),(display_height/2))
+				gamedisplays.blit(textsurf,textrect)
+				pygame.display.update()
+				time.sleep(3)
 
 		if y<obs_starty+obs_height:
 			if x>obs_startx and x<obs_startx+obs_width or x+car_width>obs_startx and x+car_width<obs_startx+obs_width:
@@ -129,4 +154,4 @@ def game_loop():
 
 game_loop()
 pygame.quit()
-quit()
+quit() 
